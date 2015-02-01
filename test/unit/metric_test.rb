9d2f618fb7816a729eb9424bbe0c1f999fb0ee96
Dan Kozlowski <dank@invoca.com>
2015-01-31 20:42:08 -0800

    Better tests around dates.

--- test/unit/metric_test.rb
+++ test/unit/metric_test.rb
@@ -16,11 +16,19 @@ describe Metric  do
       it "should be returned as a Date" do
         assert_kind_of Date, @metric.start_date
       end
+
+      it "should reflect a base date of 2009/01/01" do
+        assert_equal Date.new(2009, 1, 2), @metric.start_date
+      end
     end
 
     describe '#end_date' do
       it "should be returned as a Date" do
         assert_kind_of Date, @metric.end_date
+      end
+
+      it "should reflect a base date of 2009/01/01" do
+        assert_equal Date.new(2009, 1, 2), @metric.end_date
       end
     end
 
@@ -28,6 +36,9 @@ describe Metric  do
       it "should be returned as a DateTime" do
         assert_kind_of DateTime, @metric.last_calculated_at
       end
+    end
+
+    describe '#to_hash' do
     end
   end
 end
