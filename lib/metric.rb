49f6cd691bd56b82d0762f14e9b528f4f979a776
Dan Kozlowski <dank@invoca.com>
2015-01-31 18:34:59 -0800

    Spiked get args.

--- lib/metric.rb
+++ lib/metric.rb
@@ -2,9 +2,12 @@
 class Metric
   require 'date'
 
+  attr_reader :metric_id, :value
+
   def initialize(metric_id:, start_date:, time_range_length:, value:, last_calculated_at:, end_date:)
     @metric_id          = metric_id.to_i
     @start_date         = start_date.to_i
+    @value              = value
     @time_range_length  = time_range_length.to_i
     @last_calculated_at = last_calculated_at.to_i
     @end_date           = end_date.to_i
@@ -24,10 +27,11 @@ class Metric
 
   def to_hash
     {metric_id: @metric_id,
-     start_time: @start_date,
-     time_length_range: @time_length_range,
-     last_calculated_at: @last_calculated_at,
-     end_date: @end_date}
+     start_date: start_date.to_s,
+     time_length_range: @time_range_length,
+     value: @value,
+     last_calculated_at: last_calculated,
+     end_date: end_date.to_s}
   end
 
 end
--- lib/metrics_manager.rb
+++ lib/metrics_manager.rb
@@ -19,16 +19,29 @@ class MetricsManager
   end
 
   def get(query_string)
-    @results = @metrics
+    @result = @metrics
 
     if !query_string.empty?
-
       hashed_query = CGI::parse(query_string)
 
+      if hashed_query.keys.include?("metric_id")
+        @result.delete_if { |m| m.metric_id != hashed_query["metric_id"].first.to_i }
+      end
 
+      if hashed_query.keys.include?("before_date")
+        @result.delete_if { |m| m.start_date > Date.parse(hashed_query["before_date"].first) }
+      end
+
+      if hashed_query.keys.include?("after_date")
+        @result.delete_if { |m| m.end_date < Date.parse(hashed_query["after_date"].first) }
+      end
+
+      if hashed_query.keys.include?("value")
+        @result.delete_if { |m| m.value != hashed_query["value"].first }
+      end
     end
 
-    @results.map { |r| r.to_hash }.to_json
+    @result.map { |r| r.to_hash }.to_json
 
   end
 end
